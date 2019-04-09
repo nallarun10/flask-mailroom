760d164196f65bd6084cf0352211ee931e77e428
Arun Nalla <nalla.arunkumar@gmail.com>
2019-04-09 01:45:32 -0700

    added create.jinja2 file

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from model import Donation, Donor
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 def home():
